4f2ce394c46e15074b123bdba60e0334f5850ad1
seviged <seviged@gmail.com>
2017-02-01 13:39:41 +0300

    create project

---
+++ alarmClock/alarmClock/main.c
@@ -0,0 +1,18 @@
+/*
+ * alarmClock.c
+ *
+ * Created: 01.02.2017 13:39:01
+ * Author : Seviged
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
